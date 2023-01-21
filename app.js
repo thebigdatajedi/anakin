9d9f0ec0078298b351b211a04951be6427f1c331
Gabe <cruzfg@gmail.com>
2023-01-20 17:35:24 -0700

    created bookMode for the app to pull book data from db

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://cruzfg:Butthead1@anakin.2wbl4uu.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
 //first create the router
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+//create the book model
+const Book = require('./models/bookModel');
 
 //then configure the router
 bookRouter.route('/Books')
     .get((req, res) => {
-        const response = { hello: 'This is my API' };
-        res.json(response);
-    });
+        Book.find((err, books) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.json(books);
+        });
 //then wire up the router (use the router)
-app.use('/api', bookRouter);
+        app.use('/api', bookRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon API!, Im running on port 4000 - more');
-});
-app.listen(port, () => {
-  console.log(`Running on PORT: ${port}`);
-});
+        app.get('/', (req, res) => {
+            res.send('Welcome to my Nodemon API!, Im running on port 4000 - more');
+        });
+        app.listen(port, () => {
+            console.log(`Running on PORT: ${port}`);
+        });
+    });
